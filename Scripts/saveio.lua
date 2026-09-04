aee4cae405e1298c7c69580fc002618c98213cc0
DrewCodesBadly <dhruvak@outlook.com>
2026-09-04 15:07:14 -0400

    player inventory/loadout working

--- Scripts/saveio.lua
+++ Scripts/saveio.lua
@@ -9,6 +9,7 @@
 ---@field currentCheckpointRegionTag string
 ---@field bHudVisible boolean -- genuinely no clue what this does so I'm not touching it.
 ---@field perObjectSaveData table<string, LuaPoseObjectData>
+---@field luaPlayerSaveData LuaPlayerSaveData
 LuaSaveData = {}
 
 --- FPoseObjectData mirror class.
@@ -19,6 +20,19 @@ LuaSaveData = {}
 ---@field FloatMap table<string, number>
 ---@field VectorMap table<string, FVector>
 LuaPoseObjectData = {}
+
+--- FPlayerSaveData mirror class.
+---@class LuaPlayerSaveData
+---@field UnlockedAbilityFlags integer
+---@field Chroma integer
+---@field ItemAmounts table<string, integer>
+---@field PlayerWeaponDatas table<string, table<integer, string>>
+---@field DeathLocation FVector
+---@field MapTrackedDeathLocation FVector
+---@field CorpseChroma integer
+---@field LoadoutPrimaryWeapon string
+---@field LoadoutSecondaryWeapons table<integer, string>>
+LuaPlayerSaveData = {}
 
 --- All information needed for a savestate.
 ---@class SaveState
@@ -94,8 +108,70 @@ function SaveDataToLuaTable(saveData)
         perObjData[fname:ToString()] = luaObjData
     end)
 
+    -- Player save data item amounts
+    local playerSaveItemAmounts = {}
+    saveData.PlayerSaveData.ItemAmounts:ForEach(function(key, value)
+        if not key then return end
+        ---@type UItemDataAsset
+        local itemData = key:get()
+        if not IsValid(itemData) then return end
+        local count = value:get()
+        playerSaveItemAmounts[itemData:GetFName():ToString()] = count
+    end)
+    local playerSaveWeaponDatas = {}
+    saveData.PlayerSaveData.PlayerWeaponDatas:ForEach(function(key, value)
+        print('trying to add weapon')
+        if not key then return end
+        print('key is real')
+        ---@type UWeaponDataAsset
+        local weapon = key:get()
+        ---@type FPlayerWeaponData
+        local data = value:get()
+        local affixSlots = {}
+        data.AffixSlots:ForEach(function(slotParam, affixData)
+            if not slotParam then return end
+            ---@type integer
+            local slot = slotParam:get()
+            ---@type UAffixDataAsset
+            local data = affixData:get()
+            print(IsValid(data))
+            if IsValid(data) then
+                affixSlots[slot] = data:GetFName():ToString()
+            else
+                affixSlots[slot] = ""
+            end
+        end)
+        print('adding weapon ' .. weapon:GetFName():ToString())
+        playerSaveWeaponDatas[weapon:GetFName():ToString()] = affixSlots
+    end)
+
+    -- Loadout secondary weapons
+    local playerLoadoutSecondaryWeapons = {}
+    saveData.PlayerSaveData.CurrentLoadout.SecondaryWeapons:ForEach(function(key, value)
+        if not key then return end
+        ---@type integer
+        local directionIndex = key:get()
+        ---@type UWeaponDataAsset
+        local weaponData = value:get()
+        if IsValid(weaponData) then
+            playerLoadoutSecondaryWeapons[directionIndex] = weaponData:GetFName():ToString()
+        else
+            playerLoadoutSecondaryWeapons[directionIndex] = ""
+        end
+    end)
 
     return {
+        luaPlayerSaveData = {
+            Chroma = saveData.PlayerSaveData.Chroma,
+            CorpseChroma = saveData.PlayerSaveData.CorpseChroma,
+            UnlockedAbilityFlags = saveData.PlayerSaveData.UnlockedAbilityFlags,
+            DeathLocation = saveData.PlayerSaveData.DeathLocation,
+            MapTrackedDeathLocation = saveData.PlayerSaveData.MapTrackedDeathLocation,
+            ItemAmounts = playerSaveItemAmounts,
+            PlayerWeaponDatas = playerSaveWeaponDatas,
+            LoadoutPrimaryWeapon = saveData.PlayerSaveData.CurrentLoadout.PrimaryWeapon:GetFName():ToString(),
+            LoadoutSecondaryWeapons = playerLoadoutSecondaryWeapons,
+        },
         currentCheckpointName = saveData.CurrentCheckpointID:ToString(),
         currentCheckpointRegionTag = saveData.CurrentCheckpointRegionTag.TagName:ToString(),
         bHudVisible = saveData.bHUDVisible,
@@ -138,36 +214,88 @@ function LoadLuaData(luaData, saveData)
             LoadDataIntoFNameMap(objData.VectorMap, luaObjData.VectorMap)
             objData.FlagSet:Empty()
             for i, flagName in ipairs(luaObjData.FlagSet) do
-                print('adding flag ' .. flagName)
                 objData.FlagSet:Add(FName(flagName))
             end
         end
     end)
-
     for i, v in ipairs(toRemove) do
         saveData.PerObjectSaveData:Remove(v)
     end
-    -- for k, v in pairs(luaData.perObjectSaveData) do
-    --     print('creating new obj data')
-    -- ---@type FPoseObjectData
-    -- local objData = Construct("/Script/Pose.PoseObjectData", saveData, "saveState_objData_" .. k)
-    -- for name, bool in pairs(v.BoolMap) do
-    --     objData.BoolMap:Add(FName(name), bool)
-    -- end
-    -- for name, int in pairs(v.IntMap) do
-    --     objData.IntMap:Add(FName(name), int)
-    -- end
-    -- for name, float in pairs(v.FloatMap) do
-    --     objData.FloatMap:Add(FName(name), float)
-    -- end
-    -- for name, vec in pairs(v.VectorMap) do
-    --     objData.VectorMap:Add(FName(name), vec)
-    -- end
-    -- for i = 1, #v.FlagSet, 1 do
-    --     objData.FlagSet:Add(FName(v.FlagSet[i]))
-    -- end
-    -- saveData.PerObjectSaveData:Add(FName(k), v) -- ue4ss should convert this table automatically
-    -- end
+
+    -- Item/Weapon data
+    -- Requires getting item objects from the UItemDataProvider
+    ---@type UItemDataProvider
+    local itemDataProvider = FindFirstOf("ItemDataProvider")
+    if IsValid(itemDataProvider) then
+        print('yippeeee')
+        -- Item data
+        saveData.PlayerSaveData.ItemAmounts:Empty()
+        for k, v in pairs(luaData.luaPlayerSaveData.ItemAmounts) do
+            print('adding data: item ' .. k .. ' x' .. v)
+            local data = itemDataProvider:FindItemByName(0, FName(k))
+            print(IsValid(data))
+            saveData.PlayerSaveData.ItemAmounts:Add(data, v)
+        end
+        -- Weapon data
+        -- Cannot create weapon data so again this can only go backwards
+        -- First remove data
+        -- reusing old i and toRemove
+        i = 1
+        toRemove = {}
+        saveData.PlayerSaveData.PlayerWeaponDatas:ForEach(function(weaponParam, data)
+            ---@type UWeaponDataAsset
+            local weapon = weaponParam:get()
+            if not luaData.luaPlayerSaveData.PlayerWeaponDatas[weapon:GetFName():ToString()] then
+                print('Adding weapon ' .. weapon:GetFName():ToString() .. ' to remove list')
+                toRemove[i] = weapon
+                i = i + 1
+            end
+        end)
+        for index, weaponData in ipairs(toRemove) do
+            print('removing a weapon')
+            saveData.PlayerSaveData.PlayerWeaponDatas:Remove(weaponData)
+        end
+
+        -- Now set affix slots on remaining data
+        for k, v in pairs(luaData.luaPlayerSaveData.PlayerWeaponDatas) do
+            local data = itemDataProvider:FindWeaponByName(0, FName(k))
+            print(IsValid(data))
+            if saveData.PlayerSaveData.PlayerWeaponDatas:Contains(data) then
+                local weaponData = saveData.PlayerSaveData.PlayerWeaponDatas:Find(data)
+                -- The return type on Find is wrong, you need to call get() first.
+                local affixSlots = weaponData:get().AffixSlots
+                print('emptied affix slots for weapon ' .. k)
+                affixSlots:Empty()
+                for slot, affixName in pairs(v) do
+                    print('adding an affix ' .. affixName .. ' back to weapon ' .. k)
+                    if affixName == "" then
+                        affixSlots:Add(slot, CreateInvalidObject())
+                    else
+                        affixSlots:Add(slot, itemDataProvider:FindAffixByName(FName(affixName)))
+                    end
+                end
+            end
+        end
+        -- Set loadout
+        saveData.PlayerSaveData.CurrentLoadout.PrimaryWeapon = itemDataProvider:FindWeaponByName(0,
+            FName(luaData.luaPlayerSaveData.LoadoutPrimaryWeapon))
+        for k, v in pairs(luaData.luaPlayerSaveData.LoadoutSecondaryWeapons) do
+            if v == "" then
+                saveData.PlayerSaveData.CurrentLoadout.SecondaryWeapons:Add(k, CreateInvalidObject())
+            else
+                saveData.PlayerSaveData.CurrentLoadout.SecondaryWeapons:Add(k,
+                    itemDataProvider:FindWeaponByName(0, FName(v)))
+            end
+        end
+    end
+
+
+    -- Misc player data
+    saveData.PlayerSaveData.Chroma = luaData.luaPlayerSaveData.Chroma
+    saveData.PlayerSaveData.CorpseChroma = luaData.luaPlayerSaveData.CorpseChroma
+    saveData.PlayerSaveData.DeathLocation = luaData.luaPlayerSaveData.DeathLocation
+    saveData.PlayerSaveData.MapTrackedDeathLocation = luaData.luaPlayerSaveData.MapTrackedDeathLocation
+    saveData.PlayerSaveData.UnlockedAbilityFlags = luaData.luaPlayerSaveData.UnlockedAbilityFlags
 
     return saveData
 end
